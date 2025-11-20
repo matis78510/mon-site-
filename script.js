0cf284c56b158bf25ff89936a31465b3e90a72c1
matis78510 <matis.rodrigues@ileps.fr>
2025-11-20 11:29:30 +0100

    Implement carousel with automatic slide transition
    Added carousel functionality to the script.

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Merci pour votre message!');
-});
+// Carrousel
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const totalSlides = slides.length;
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateCarousel();
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+}
+
+function updateCarousel() {
+    const newTransformValue = -currentSlide * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${newTransformValue}%)`;
+}
+
+// Lancer le carrousel toutes les 5 secondes
+setInterval(nextSlide, 5000);
